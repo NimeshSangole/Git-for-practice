5e433ae64873ad34396c8df897e13fc5fa4e3088
nimesh sangole <nimeshsangole007@gmail.com>
2022-05-15 01:17:28 +0530

    Initial commit

---
+++ src/org/phone/PhoneInfo.java
@@ -0,0 +1,33 @@
+package org.phone;
+
+public class PhoneInfo {
+	public void phoneName()
+	{
+		System.out.println("phone name is apple");
+	}
+	public void phoneMieiNum()
+	{
+		System.out.println("phone miei number is 123456789");
+	}
+	public void camera()
+	{
+		System.out.println("camera is 48 mega pixel");
+	}
+	public void storage()
+	{
+		System.out.println("storage is 128gb");
+	}
+	public void osName()
+	{
+		System.out.println("OS name is Windows");
+	}
+	public static void main(String[] args) {
+		PhoneInfo s=new PhoneInfo();
+		s.phoneName();
+		s.phoneMieiNum();
+		s.camera();
+		s.storage();
+		s.osName();
+	}
+
+}
